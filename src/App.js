2607e9803d61aa14522e1db5ecf481e0c2f1f3d3
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-18 19:22:01 -0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(response => setRepositories(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post('/repositories', {
+        url: 'https://github.com/ricardoaruiz/gostack2020-N01D02-frontend-reactjs',
+        title: `Desafio: Conceitos do ReactJS ${Date.now()}`,
+        techs: ['Javascript', 'ReactJS']
+      });
+      setRepositories([ ...repositories, response.data ]);
+    } catch(error) {
+      console.lerror(error);
+      alert('Problema ao cadastrar um novo repositório. Tente mais tarde');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      setRepositories(repositories.filter(repository => repository.id !== id));      
+    } catch(error) {
+      console.lerror(error);
+      alert('Problema ao remover um repositório. Tente mais tarde');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button 
+              type="button"
+              onClick={() => handleRemoveRepository(repository.id)}
+            >
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
